560f6a967a7278cfbd7610a46ba9d2eef1c88a02
abeljlim <subrosian@hotmail.com>
2023-04-12 21:14:29 -0700

    Commented out my old version of the code in favour of fs.access which hopefully would result in cleaner code

--- fetcher.js
+++ fetcher.js
@@ -1,4 +1,4 @@
-const { stdout } = require('process');
+// const { stdout } = require('process');
 const request = require('request');
 const args = process.argv.slice(2);
 
@@ -6,51 +6,99 @@ const srcURL = args[0];
 const destPath = args[1];
 
 const fs = require('fs');
+// const { error } = require('console');
 
-const writeToFile = (destPath, text) => {
-  // write to file
-  const content = text;
-
-  fs.writeFile(destPath, content, err => {
-    if (err) {
-      console.error(err);
+const downloader = function (sourceUrl, destPath) {
+  request(sourceUrl, (error, response, body) => {
+    if(error) {
+      throw error;
     }
-    // file written successfully
-    // note: 1 character is equal to 1 byte, so text.length gets the size in bytes
-    console.log(`Downloaded and saved ${text.length} bytes to ${destPath}`);
-  });
-};
+    
+    // if destPath already exists
+    fs.access(destPath, (error) => {
+      if(error) {
+        console.log(error);
+        throw error;
+      }
+      console.log(`File exists`);
+    });
 
-// read from URL
-request(srcURL, (error, response, body) => {
-  if (error) {
-    console.log("error:", error);
-    return;
-  }
+    fs.writeFile(destPath, body, err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(`Downloaded ${body.length} bytes`);
+    });
+  })
+}
+downloader(srcURL, destPath);
+// const writeToFile = (destPath, text) => {
+//   // write to file
+//   const content = text;
 
-  // check if file already exists
-  fs.stat(destPath, (err, stat) => {
-    if(err === null) {
-      // prompt user if they want to overwrite the file
-      const readline = require('readline');
+//   fs.writeFile(destPath, content, err => {
+//     if (err) {
+//       console.error(err);
+//     }
+//     // file written successfully
+//     // note: 1 character is equal to 1 byte, so text.length gets the size in bytes
+//     console.log(`Downloaded and saved ${text.length} bytes to ${destPath}`);
+//   });
+// };
 
-      const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-      });
+// // Edge case 3: If the URL is invalid
+// const isValidHttpUrl = string => {
+//   let url;
+//   try {
+//     url = new URL(string);
+//   } catch (_) {
+//     return false;
+//   }
+//   return url.protocol === "http:" || url.protocol === "https:";
+// };
 
-      rl.question(`File at ${destPath} found! Do you want to overwrite it? (type Y if so) `, (answer) => {
-        if (answer !== 'Y') {
-          rl.close();
-          return;
-        }
-        // Happy path in this callback waterfall
-        writeToFile(destPath, body);
-        rl.close();
-      });
-      return;
-    }
-    // Happy path - file does not exist so err.code === 'ENOENT' or something else
-    writeToFile(destPath, body);
-  });
-});
+// if (isValidHttpUrl(srcURL)) {
+//   // read from URL
+//   // request(srcURL, (error, response, body) => {
+//   //   if (error) {
+//   //     console.log("error:", error);
+//   //     return;
+//   //   }
+
+//   //   // Edge case 1: check if file already exists
+//   //   fs.stat(destPath, (err, stat) => {
+//   //     if (err === null) {
+//   //       // prompt user if they want to overwrite the file
+//   //       const readline = require('readline');
+
+//   //       const rl = readline.createInterface({
+//   //         input: process.stdin,
+//   //         output: process.stdout
+//   //       });
+
+//   //       rl.question(`File at ${destPath} found! Do you want to overwrite it? (type Y if so) `, (answer) => {
+//   //         if (answer !== 'Y') {
+//   //           rl.close();
+//   //           return;
+//   //         }
+//   //         // Happy path in this callback waterfall
+//   //         writeToFile(destPath, body);
+//   //         rl.close();
+//   //       });
+//   //       return;
+//   //     }
+//   //     // Happy path - file does not exist so err.code === 'ENOENT' or something else
+
+//   //     // Edge case 2: If the path is invalid (found from stackoverflow, but this doesn't seem to always work)
+//   //     if (!(/^(\/?[a-z0-9]+)+$/.test(destPath))) {
+//   //       console.log("Error: Invalid file path.");
+//   //       return;
+//   //     }
+//   //     writeToFile(destPath, body);
+//   //   });
+//   // });
+// }
+// else {
+//   console.log("Error: URL is Invalid");
+// }
