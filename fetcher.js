8bc0b7a9b42a691e4f1e2294debe113bad942d5f
abeljlim <subrosian@hotmail.com>
2023-04-12 20:23:35 -0700

    Stretch Edge Case 1

--- fetcher.js
+++ fetcher.js
@@ -1,8 +1,25 @@
+const { stdout } = require('process');
 const request = require('request');
 const args = process.argv.slice(2);
 
 const srcURL = args[0];
 const destPath = args[1];
+
+const fs = require('fs');
+
+const writeToFile = (destPath, text) => {
+  // write to file
+  const content = text;
+
+  fs.writeFile(destPath, content, err => {
+    if (err) {
+      console.error(err);
+    }
+    // file written successfully
+    // note: 1 character is equal to 1 byte, so text.length gets the size in bytes
+    console.log(`Downloaded and saved ${text.length} bytes to ${destPath}`);
+  });
+};
 
 // read from URL
 request(srcURL, (error, response, body) => {
@@ -10,21 +27,28 @@ request(srcURL, (error, response, body) => {
     console.log("error:", error);
     return;
   }
-  
-  // write to file
 
-  const fs = require('fs');
+  // check if file already exists
+  fs.stat(destPath, (err, stat) => {
+    if(err === null) {
+      // prompt user if they want to overwrite the file
+      const readline = require('readline');
 
-  const content = body;
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
 
-  fs.writeFile(destPath, content, err => {
-    if (err) {
-      console.error(err);
+      rl.question(`File at ${destPath} found! Do you want to overwrite it? (type Y if so) `, (answer) => {
+        if (answer !== 'Y') {
+          return;
+        }
+        // Happy path in this callback waterfall
+        writeToFile(destPath, body);
+      });
+      return;
     }
-    // file written successfully
+    // Happy path
+    writeToFile(destPath, body);
   });
-  
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
 });
